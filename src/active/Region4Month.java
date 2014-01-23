cb964f2b6814567d76525b22fd1c121981f88966
huangxr <reesun.huang@gmail.com>
2014-01-23 13:42:11 +0800

    按月查询地区

--- src/active/Region4Month.java
+++ src/active/Region4Month.java
@@ -7,12 +7,12 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
-import beans.SumRegionByDay;
 import utils.DBHandler;
 import utils.DateUtils;
 import utils.handler.DateSKHandler;
+import beans.SumRegionByDay;
 
-public class Region4Day {
+public class Region4Month {
 	public static void main(String[] args) throws Exception {
 		Connection conn = DBHandler.getConnection();
 		if (conn == null)
@@ -23,27 +23,28 @@ public class Region4Day {
 		String query = "SELECT date_sk FROM  razor_dim_date WHERE datevalue=?";
 		QueryRunner run = new QueryRunner();
 
-		String dateValue = DateUtils.getYesterday();
-		Integer datesk = run.query(conn, query, dateHandler, dateValue);
+		String dateValue = DateUtils.getLastMonth();
+		Integer datesk = run.query(conn, query, dateHandler, dateValue+"-01");
+		Integer endDatesk = datesk + 30;
 
 		query = "SELECT '"
 				+ dateValue
 				+ "' as datetime, region, city, count(distinct(deviceidentifier)) as num "
 				+ " FROM razor_fact_clientdata f join  razor_dim_location  l"
-				+ " on f.location_sk = l.location_sk" + " where date_sk = ? "
+				+ " on f.location_sk = l.location_sk" + " where date_sk >= ? and date_sk <= ?"
 				+ " group by f.location_sk";
 
-//		System.out.println(query);
+		// System.out.println(query);
 
 		ResultSetHandler<List<SumRegionByDay>> regionHandler = new BeanListHandler<SumRegionByDay>(
 				SumRegionByDay.class);
-		List<SumRegionByDay> regions = run.query(conn, query, regionHandler,
-				datesk);
+		List<SumRegionByDay> regions = run.query(conn, query, regionHandler,datesk, endDatesk);
 
-		query = "insert into razor_sum_region_by_day (datetime,region,city,num) values (?,?,?,?)";
+		query = "insert into razor_sum_region_by_month (datetime,region,city,num) values (?,?,?,?)";
 
 		for (SumRegionByDay region : regions) {
-			// System.out.println(region.getDatetime()+" "+region.getRegion()+" "+ region.getCity()+ " " + region.getNum());
+			// System.out.println(region.getDatetime()+" "+region.getRegion()+" "+
+			// region.getCity()+ " " + region.getNum());
 			run.update(conn, query, region.getDatetime(), region.getRegion(),
 					region.getCity(), region.getNum());
 		}
--- src/utils/DateUtils.java
+++ src/utils/DateUtils.java
@@ -8,7 +8,17 @@ public class DateUtils {
 	public static String getYesterday() {
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
 		Calendar rightNow = Calendar.getInstance();
-		rightNow.add(Calendar.DAY_OF_YEAR,-1);//日期加10天
+		rightNow.add(Calendar.DAY_OF_YEAR,-1); // 前一天
+		Date dt1=(Date) rightNow.getTime();
+		
+		return sdf.format(dt1);
+	
+	}
+	
+	public static String getLastMonth() {
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM");
+		Calendar rightNow = Calendar.getInstance();
+		rightNow.add(Calendar.MONTH,-1);
 		Date dt1=(Date) rightNow.getTime();
 		
 		return sdf.format(dt1);
