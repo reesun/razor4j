7e189889ddfbb1642b6b5e92a42a7874769dcc53
huangxr <reesun.huang@gmail.com>
2014-01-23 10:29:30 +0800

    格式化代码

--- src/active/Region4Day.java
+++ src/active/Region4Day.java
@@ -12,47 +12,43 @@ import utils.DBHandler;
 import utils.DateUtils;
 import utils.handler.DateSKHandler;
 
-
-
 public class Region4Day {
-	public static String getDateValue() {
-
-		return null;
-	}
-
-	
-	
 	public static void main(String[] args) throws Exception {
 		Connection conn = DBHandler.getConnection();
-		if(conn == null)
+		if (conn == null)
 			return;
-		
+
 		ResultSetHandler<Integer> dateHandler = new DateSKHandler();
-		
+
 		String query = "SELECT date_sk FROM  razor_dim_date WHERE datevalue=?";
 		QueryRunner run = new QueryRunner();
-		
+
 		String dateValue = DateUtils.getYesterday();
 		Integer datesk = run.query(conn, query, dateHandler, dateValue);
-		
-		query = "SELECT '"+dateValue+"' as datetime, region, city, count(distinct(deviceidentifier)) as num "
+
+		query = "SELECT '"
+				+ dateValue
+				+ "' as datetime, region, city, count(distinct(deviceidentifier)) as num "
 				+ " FROM razor_fact_clientdata f join  razor_dim_location  l"
-				+ " on f.location_sk = l.location_sk"
-				+ " where date_sk = ? "
+				+ " on f.location_sk = l.location_sk" + " where date_sk = ? "
 				+ " group by f.location_sk";
-		
+
 		System.out.println(query);
-		
-		ResultSetHandler<List<SumRegionByDay>> regionHandler = new BeanListHandler<SumRegionByDay>(SumRegionByDay.class);
-		List<SumRegionByDay> regions = run.query(conn, query, regionHandler, datesk);
-		
+
+		ResultSetHandler<List<SumRegionByDay>> regionHandler = new BeanListHandler<SumRegionByDay>(
+				SumRegionByDay.class);
+		List<SumRegionByDay> regions = run.query(conn, query, regionHandler,
+				datesk);
+
 		query = "insert into razor_sum_region_by_day (datetime,region,city,num) values (?,?,?,?)";
-		
-		for(SumRegionByDay region:regions){
-//			System.out.println(region.getDatetime()+" "+region.getRegion()+" " + region.getCity()+ " " + region.getNum());
-			run.update(conn, query, region.getDatetime(),region.getRegion(),region.getCity(),region.getNum());
+
+		for (SumRegionByDay region : regions) {
+			// System.out.println(region.getDatetime()+" "+region.getRegion()+" "
+			// + region.getCity()+ " " + region.getNum());
+			run.update(conn, query, region.getDatetime(), region.getRegion(),
+					region.getCity(), region.getNum());
 		}
-		
-		System.out.println(dateValue+" down!");
+
+		System.out.println(dateValue + " down!");
 	}
 }
